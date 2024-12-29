f490d4fbc94badec126d7fc410335cc93cc46fae
lucifer <piyushkhurana@devit-mac.local>
2024-12-29 17:30:38 +0530

    done with day02 part2

--- 2024/day02/day02.pl
+++ 2024/day02/day02.pl
@@ -1,10 +1,12 @@
 #!/opt/local/bin/perl
-use Text::CSV_XS;
+use strict;
+use warnings;
 
 my $file = 'input.txt';
 open my $fh, '<', $file or die "Could not open '$file': $!";
 
 my $count = 0;
+my @unsafe_list;
 
 while (my $line = <$fh>) {
 	chomp $line;
@@ -33,91 +35,54 @@ while (my $line = <$fh>) {
 
 	if ($flag == 1) {
 		$count++;
-	}
+	} else {
+        push @unsafe_list, \@list;
+    }
 }
 
 print("Part 1 : ", $count);
 print("\n");
 
 close $fh;
-open $fh, '<', $file or die "Could not open '$file': $!";
-
-$count = 0;
-my @unsafe_list;
-while (my $line = <$fh>) {
-	chomp $line;
-	my @list = split ' ', $line;
-	my $flag = 1;
-	my $diff = 0;
-	if($list[1] > $list[0]) {
-		for my $i (0 .. $#list-1) {
-			$diff = $list[$i+1]-$list[$i];
-			if ($list[$i+1] < $list[$i] || $diff > 3 || $diff < 1) {
-				splice(@list, $i, 1);
-				$flag=0;
-				last;
-			}
-		}
-	} elsif ($list[1] < $list[0]) {
-		for my $i (0 .. $#list-1) {
-			$diff = $list[$i]-$list[$i+1];
-			if ($list[$i+1] > $list[$i] || $diff > 3 || $diff < 1) {
-				splice(@list, $i, 1);
-				$flag=0;
-				last;
-			}
-		}
-	} else {
-		$flag=0;
-		splice(@list, 0, 1);
-	}
-
-	if ($flag == 1) {
-		$count++;
-	}
-	else {
-		push @unsafe_list, \@list;
-	}
-}
-
-close $fh;
 
 for my $k (0 .. $#unsafe_list) {
-	my $list = $unsafe_list[$k];
-	for my $j (0 .. $#list-1) {
-		print($list[$j], " ");
-	}
-	print("\n");
+    my $list_ref = $unsafe_list[$k];  
+	my @list = @$list_ref;
 	my $flag = 1;
 	my $diff = 0;
-	if($list[1] > $list[0]) {
-		for my $i (0 .. $#list-1) {
-			$diff = $list[$i+1]-$list[$i];
-			if ($list[$i+1] < $list[$i] || $diff > 3 || $diff < 1) {
-				$flag=0;
-				last;
-			}
+	for my $col_idx (0 .. $#list) {
+        my $removed = splice(@list, $col_idx, 1);
+        if($list[1] > $list[0]) { 
+	        $flag = 1;
+            for my $i (0 .. $#list-1) {
+                $diff = $list[$i+1]-$list[$i];
+                if ($list[$i+1] < $list[$i] || $diff > 3 || $diff < 1) {
+                    $flag=0;
+                    last;
+                }
+            }
+            if($flag == 1) {
+                $count++;
+                last;
+            }
+        } elsif ($list[1] < $list[0]) {
+            $flag = 1;
+            for my $i (0 .. $#list-1) {
+                $diff = $list[$i]-$list[$i+1];
+                if ($list[$i+1] > $list[$i] || $diff > 3 || $diff < 1) {
+                    $flag=0;
+                    last;
+                }
+            }
+            if($flag == 1) {
+                $count++;
+                last;
+            }
 		}
-	} elsif ($list[1] < $list[0]) {
-		for my $i (0 .. $#list-1) {
-			$diff = $list[$i]-$list[$i+1];
-			if ($list[$i+1] > $list[$i] || $diff > 3 || $diff < 1) {
-				$flag=0;
-				last;
-			}
-		}
-	} else {
-		$flag=0;
-	}
-
-	if ($flag == 1) {
-		$count++;
-	}
+        splice(@list, $col_idx, 0, $removed);
+	} 
 }
 
-print("Part 2 : ", $count);
-print("\n");
-
-exit 0;
+print("Part 2 : $count \n");
 
 
