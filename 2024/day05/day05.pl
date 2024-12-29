2ed34c75b86964be388538fed22ec60734d9967c
lucifer <piyushkhurana@devit-mac.local>
2024-12-29 18:01:44 +0530

    done with day05

--- 2024/day05/day05.pl
+++ 2024/day05/day05.pl
@@ -2,44 +2,72 @@
 use strict;
 use warnings;
 
-my $filename = 'sample1.txt';
+my $filename = 'input.txt';
 open(my $fh, '<', $filename) or die "Could not open file '$filename': $!";
 
-my @list1; # n*2 list
-my @list2; # n*2 list
+my @list;
+my @updates;
+my @incorrect_updates;
 
 while (my $line = <$fh>) {
     chomp $line;
-    my ($num1, $num2) = split /\|/, $line;
-    push @list1, $num1;
-    push @list2, $num2;
+    if ($line =~ /^\s*$/) {
+        last;
+    }
+    push @list, $line;
+}
+
+while(my $line = <$fh>) {
+    chomp $line;
+    push @updates, $line;
 }
 
 close $fh;
 
-$filename = 'sample2.txt';
-open($fh, '<', $filename) or die "Could not open file '$filename': $!";
-
-my @updates; # 2d list
-
-while (my $line = <$fh>) {
-    chomp $line;
-    my @nums = split /,/, $line;
-    push @updates, \@nums;
-}
-
-close $fh;
-
+my $sum = 0;
 for my $i (0 .. $#updates) {
-	my @line = @{$updates[$i]};
-	my $j = 0;
-	while ($j < $#line) {
-		print("finding ", $line[$j], " in list1", "\n");
-		my @indexes = grep { $list1[$_] == $line[$j] } 0..$#list1;
-		for my $k (0..$#indexes) {
-
-		}
-		print("\n");
-		$j++;
-	}
+    my @line = split ',', $updates[$i];
+    my $flag=1;
+    for my $j (0 .. $#line) {
+        for my $k ($j+1 .. $#line) {
+            my $incorrect_rule = $line[$k]."|".$line[$j];
+		    my @indexes = grep { $list[$_] eq $incorrect_rule } 0..$#list;
+            if ($#indexes > -1) {
+                $flag=0;
+                last;
+            }
+        }
+        if ($flag == 0) {
+            last;
+        }
+    }
+    if ($flag == 1) {
+        $sum += $line[$#line/2];
+    }
+    else {
+        push @incorrect_updates, $updates[$i];
+    }
 }
+
+print("Part 1: $sum\n");
+$sum = 0;
+
+for my $i (0 .. $#incorrect_updates) {
+    my @line = split ',', $incorrect_updates[$i];
+    my $flag=1;
+    for my $j (0 .. $#line) {
+        for my $k ($j+1 .. $#line) {
+            my $incorrect_rule = $line[$k]."|".$line[$j];
+		    my @indexes = grep { $list[$_] eq $incorrect_rule } 0..$#list;
+            if ($#indexes > -1) {
+                my $temp = $line[$k];
+                $line[$k] = $line[$j];
+                $line[$j] = $temp;
+            }
+        }
+    }
+    $sum += $line[$#line/2];
+}
+
+
+print("Part 2: $sum\n");
