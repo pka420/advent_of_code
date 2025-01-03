f26d89491e2f34ead461d37404cf43b5a8c2ee49
lucifer <piyushkhurana@devit-mac.local>
2025-01-03 14:29:15 +0530

    WIP day 12

--- 2024/day12/day12.pl
+++ 2024/day12/day12.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-my $filename = 'sample4.txt';
+my $filename = 'sample.txt';
 open(my $fh, '<', $filename) or die "Could not open file '$filename': $!";
 
 my @map;
@@ -77,63 +77,68 @@ sub part1 {
 #
 }
 
+sub is_vertex {
+    my ($row, $col) = @_;
+    my $char = $map[$row][$col];
+
+
+    my $top = ($row > 0) ? $map[$row-1][$col] : undef;
+    my $bottom = ($row < $rows-1) ? $map[$row+1][$col] : undef;
+    return 0 if defined $top && defined $bottom && $top eq $char && $bottom eq $char;
+
+    my $left = ($col > 0) ? $map[$row][$col-1] : undef;
+    my $right = ($col < $cols-1) ? $map[$row][$col+1] : undef;
+    return 0 if defined $left && defined $right && $left eq $char && $right eq $char;
+
+    return 1;
+}
+
+
 sub explore_region2 {
     my ($row, $col, $char, $visited) = @_;
     
-    return (0, 0) if $row < 0 || $row >= $rows || $col < 0 || $col >= $cols; 
-    return (0, 0) if $visited->{"$row,$col"};
-    return (0, 0) if $map[$row][$col] ne $char;  
+    return (0,0) if $row < 0 || $row >= $rows || $col < 0 || $col >= $cols; 
+    return (0,0) if $visited->{"$row,$col"};
+    return (0,0) if $map[$row][$col] ne $char;  
 
-    $map[$row][$col] = '-';
     $visited->{"$row,$col"} = 1;
 
     my $area = 1;
-    my $sides = 4;
+    my $bounds = is_vertex($row, $col);
 
     foreach my $dir (@directions) {
         my $new_row = $row + $dir->[0];
         my $new_col = $col + $dir->[1];
         
-        if ($new_row >= 0 && $new_row < $rows && $new_col >= 0 && $new_col < $cols && $map[$new_row][$new_col] eq $char) {
-            $sides -= 3; 
-        }
-        my ($d_area, $d_sides) = explore_region2($new_row, $new_col, $char, $visited);
+        my ($d_area, $d_bounds) = explore_region2($new_row, $new_col, $char, $visited);
         $area += $d_area;
-        $sides += $d_sides;
-
+        $bounds += $d_bounds;
     }
-    return ($area, $sides);
+    return ($area, $bounds);
 }
 
 sub part2 {
     my %area;
-    my %perimeter;
+    my %sides;
     my $sum = 0;
+    my %visited;
     for my $i (0 .. $#map) {
         for my $j (0 .. $#{$map[$i]}) {
             my $char = $map[$i][$j];
             if ($char ne '-') {
-                my %visited;
-                my ($ar, $pr) = explore_region2($i, $j, $char, \%visited);
-                # print("visited for $char \n");
-                # for my $element (keys %visited) {
-                #     print "$element, ";
-                # }
-                # print("\n");
-                if ($pr > 4) {
-                    $pr -= 4;
-                }
-                 
-                $sum += $ar*$pr;
+                my ($ar, $sd) = explore_region2($i, $j, $char, \%visited );
+
+                $sum += $ar*$sd;
 
                 $area{$char} += $ar;
-                $perimeter{$char} += $pr;
+                $sides{$char} += $sd;
+                print "$char area => $ar, sides => $sd \n";
             }
         }
     }
     print("sum: ", $sum, "\n");
     for my $char (keys %area) {
-        print "$char area => $area{$char}, perimeter => $perimeter{$char} \n";
+        print "$char area => $area{$char}, sides => $sides{$char} \n";
     }
 }
 part2();
