03bfaff1b8beea64f09427051c86870f6a33a054
Piyush Khurana <piyushkhurana38@gmail.com>
2025-01-04 01:44:17 +0530

    done day14

--- 2024/day14/day14.pl
+++ 2024/day14/day14.pl
@@ -43,51 +43,47 @@ sub part1 {
         $quadrants[3]++ if $x > $mid_w && $y > $mid_h;
     }
 
-    for my $i (0 .. 3) {
-        print("quad $i: $quadrants[$i] \n");
-    }
-
     my $safety = $quadrants[0]*$quadrants[1]*$quadrants[2]*$quadrants[3];
     print("safety_factor: $safety \n");
+    return $safety;
 }
 
 #part1();
-#
-
-sub check_christmas_tree {
-    for my $i (0 .. $width) {
-        for my $j (0 .. $height) {
-            my $p = 0;
-            for my $k (0 .. $#position) {
-                if ($position[$k][0] == $i && $position[$k][1] == $j) {
-                    print "#";
-                    $p=1;
-                    last;
-                }
-            }
-            print "." if $p==0;
-        }
-        print("\n");
-    }
-}
 
 sub part2 {
     my $seconds = 1;
-    while($seconds<50) {
+    my $flag = 1;
+    my $mid_w = int(($width-1)/2);
+    my $mid_h = int(($height-1)/2);
+    my $min_safety = 130686500;
+    # previous answer's changed 2 to 1.
+    my $best_seconds = $seconds;
+
+    my $x;
+    my $y;
+    while($seconds < 10000) {
+        my @quadrants = (0,0,0,0);
         for my $i (0 .. $#position) {
-            my ($x, $y) = ($position[$i][0], $position[$i][1]);
+            ($x, $y) = ($position[$i][0], $position[$i][1]);
             my ($vx, $vy) = ($velocity[$i][0], $velocity[$i][1]);
 
-            my $seconds = 1;
-            $position[$i][0] = ($x + $vx*$seconds)%$width;
-            $position[$i][1] = ($y + $vy*$seconds)%$height;
-            ($x, $y) = ($position[$i][0], $position[$i][1]);
+            $x = ($x + $vx*$seconds)%$width;
+            $y = ($y + $vy*$seconds)%$height;
+            $quadrants[0]++ if $x < $mid_w && $y < $mid_h;
+            $quadrants[1]++ if $x < $mid_w && $y > $mid_h;
+            $quadrants[2]++ if $x > $mid_w && $y < $mid_h;
+            $quadrants[3]++ if $x > $mid_w && $y > $mid_h;
         }
-        print("After $seconds seconds:\n");
-        last if check_christmas_tree();
+        my $safe = $quadrants[0]*$quadrants[1]*$quadrants[2]*$quadrants[3];
+        #print("After $seconds seconds: $safe\n");
+        if ( $safe < $min_safety){
+            $min_safety = $safe;
+            $best_seconds = $seconds;
+        }
+
         $seconds++;
     }
-
+    print("$min_safety at $best_seconds\n");
 
 }
 part2();
