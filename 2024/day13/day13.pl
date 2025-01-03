97bfa3bbeb30dd2c5d25cd0acd65cbec2628abb8
Piyush Khurana <piyushkhurana38@gmail.com>
2025-01-03 15:30:38 +0530

    done with day13

--- 2024/day13/day13.pl
+++ 2024/day13/day13.pl
@@ -1,7 +1,6 @@
 #!/opt/local/bin/perl
 use strict;
 use warnings;
-use Math::MatrixReal;
 
 sub is_integer {
     my ($num) = @_;
@@ -11,58 +10,50 @@ sub is_integer {
 my $filename = 'input.txt';
 open(my $fh, '<', $filename) or die "Could not open file '$filename': $!";
 
-my ($x1, $x2, $y1, $y2);
-my $tokens;
+my ($a0, $a1, $b0, $b1, $p0, $p1);
+my $tokens = 0;
 
 while (my $line = <$fh>) {
     chomp $line;
     next if $line =~ /^\s*$/;
 
-        print "$line \n";
-        if ($line =~ /X\+(\d+), Y\+(\d+)/) {
-            ($x1, $y1) = ($1, $2);
-        }
-        my $line2 = <$fh>;
-        chomp $line2;
-        print "$line2 \n";
-        if ($line2 =~ /X\+(\d+), Y\+(\d+)/) {
-            ($x2, $y2) = ($1, $2);
-        }
-        my $matrix = Math::MatrixReal->new_from_rows([
-            [$x1, $x2],
-            [$y1, $y2],
-        ]);
-        $line2 = <$fh>;
-        chomp $line2;
-        print "$line2 \n";
-        if ($line2 =~ /X\=(\d+), Y\=(\d+)/) {
-            ($x1, $y1) = ($1, $2);
-        }
+    print "$line \n";
+    if ($line =~ /X\+(\d+), Y\+(\d+)/) {
+        ($a0, $a1) = ($1, $2);
+    }
+    my $line2 = <$fh>;
+    chomp $line2;
+    print "$line2 \n";
+    if ($line2 =~ /X\+(\d+), Y\+(\d+)/) {
+        ($b0, $b1) = ($1, $2);
+    }
+    $line2 = <$fh>;
+    chomp $line2;
+    if ($line2 =~ /X\=(\d+), Y\=(\d+)/) {
+        ($p0, $p1) = ($1, $2);
+    }
 
-        my $vector = Math::MatrixReal->new_from_rows([
-            [$x1],
-            [$y1],
-        ]);
+    $p0+=10000000000000;
+    $p1+=10000000000000;
+    print("Prize: X=$p0, Y=$p1\n");
 
-        my ($inverse, $error) = $matrix->inverse();
-        if ( defined $error) {
-            print("matrix inverse failed");
-            next;
-        } else {
-            my $solution = $inverse*$vector;
-            $x1 = $solution->element(1,1);
-            $y1 = $solution->element(2,1);
+    my $times_b = ($p1 * $a0 - $p0 * $a1)/($b1 * $a0 - $b0 * $a1);
+    my $times_a = ($p0 - $b0 * $times_b) / $a0;
 
-            if(!is_integer($x1) || !is_integer($y1) ) {
-                next;
-            }
-
-            $tokens = $tokens + $x1*3 + $y1;
-
-            print "Solution:\n";
-            print "a = ", $x1, "\n";
-            print "b = ", $y1, "\n";
-        }
+    # if(0 <= $times_a <= 100 && 0 <= $times_b <= 100 && is_integer($times_b) && is_integer($times_a) ) {
+    #     $tokens = $tokens + $times_a*3 + $times_b;
+    #     print "Solution:\n";
+    #     print "a = ", $times_a, "\n";
+    #     print "b = ", $times_b, "\n";
+    # }
+    if(0 <= $times_a && 0 <= $times_b && is_integer($times_b) && is_integer($times_a) ) {
+        $tokens = $tokens + $times_a*3 + $times_b;
+        print "Solution:\n";
+        print "a = ", $times_a, "\n";
+        print "b = ", $times_b, "\n";
+    }
 }
 
 print"tokens: $tokens \n";
+
+close $fh;
