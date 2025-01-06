77628ed46d18804946f9811d423a84d33b23b6b7
Piyush Khurana <piyushkhurana38@gmail.com>
2025-01-06 13:09:38 +0530

    feat: done with day08

--- 2024/day08/day08.pl
+++ 2024/day08/day08.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-my $filename = 'sample.txt';
+my $filename = 'input.txt';
 open(my $fh, '<', $filename) or die "Could not open file '$filename': $!";
 
 my @map;
@@ -15,11 +15,67 @@ while (my $line = <$fh>) {
 
 close $fh;
 
-
+my %antennas;
 for my $i (0 .. $#map) {
 	for my $j (0 .. $#{$map[$i]}) {
-		print($map[$i][$j]);
+        if($map[$i][$j] ne '.') {
+            push @{$antennas{$map[$i][$j]}}, [$i, $j];
+        }
 	}
-	print("\n");
 }
 
+# foreach my $key (keys %antennas) {
+#     print("antenna $key at locations:\n");
+#     print(join(", ", map {"($_->[0], $_->[1])"} @{$antennas{$key}}));
+#     print("\n");
+# }
+
+my $rows = $#map;
+my $cols = $#{$map[0]};
+
+my %antinodes;
+foreach my $key (keys %antennas) {
+    my @coordinates = @{$antennas{$key}};
+    for my $i ( 0 .. $#coordinates) {
+        for my $j ( $i+1 .. $#coordinates) {
+            my ($r1, $c1) = ($coordinates[$i][0], $coordinates[$i][1]);
+            my ($r2, $c2) = ($coordinates[$j][0], $coordinates[$j][1]);
+            my $x = 2*$r2-$r1;
+            my $y = 2*$c2-$c1;
+            if ($x >= 0 && $x <= $rows && $y >= 0 && $y <= $cols) {
+                $antinodes{"$x,$y"} = 0;
+            }
+            $x = 2*$r1-$r2;
+            $y = 2*$c1-$c2;
+            if ($x >= 0 && $x < $rows && $y >= 0 && $y <= $cols) {
+                $antinodes{"$x,$y"} = 0;
+            }
+        }
+    }
+}
+my $size = scalar %antinodes;
+print("no of antinodes: $size \n");
+
+
+my %antinodes2;
+foreach my $key (keys %antennas) {
+    my @coordinates = @{$antennas{$key}};
+    for my $i ( 0 .. $#coordinates) {
+        for my $j ( 0 .. $#coordinates) {
+            next if $i == $j;
+            my ($r1, $c1) = ($coordinates[$i][0], $coordinates[$i][1]);
+            my ($r2, $c2) = ($coordinates[$j][0], $coordinates[$j][1]);
+            my $dr = $r2-$r1;
+            my $dc = $c2-$c1;
+            my $x = $r1;
+            my $y = $c1;
+            while(0 <= $x <= $rows && 0 <= $y <= $cols) {
+                $antinodes2{"$x,$y"} = 0;
+                $x += $dr;
+                $y += $dc;
+            }
+        }
+    }
+}
+$size = scalar %antinodes2;
+print("no of antinodes part2: $size \n");
